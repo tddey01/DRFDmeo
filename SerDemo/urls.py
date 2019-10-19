7a013e669007e2942649cf1e58b3bf73dde337c6
kevinlandun@gmail.com <1qaz2wsx3edcQA1@>
2019-10-19 17:11:30 +0800

    提交

--- SerDemo/urls.py
+++ SerDemo/urls.py
@@ -1,8 +1,11 @@
-from django.contrib import admin
+#!/usr/bin/env  python3
+# -*- coding: UTF-8 -*-
+
 from django.urls import path
 from django.urls import include
 
+from .views import BookView
 urlpatterns = [
-
+    path('list/', BookView.as_view()),
 ]
 
--- SerDemo/views.py
+++ SerDemo/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from django.shortcuts import HttpResponse
+from django.views.generic.base import View 
+from SerDemo import models
+import json
 # Create your views here.
+
+# book_list = [
+#     {
+#         'id:1,
+#         'title':'xxx',
+#     },{}
+# ]
+
+
+class BookView(View):
+    
+    def get(self,request):
+        book_list = models.Book.objects.values('id','title')   # 获取数据
+        book_list = list(book_list)
+        ret = json.dumps(book_list,ensure_ascii=False)
+        return  HttpResponse('ok')
