00a35b8c367c01f644c14ebe574e00a5bdf309ac
kevinlandun@gmail.com <1qaz2wsx3edcQA1@>
2019-10-20 14:42:56 +0800

    第三版 api接口编写

--- SerDemo/serializers.py
+++ SerDemo/serializers.py
@@ -55,7 +55,6 @@ class AuthorSerializer(serializers.Serializer):
 
 
 book_obj = {
-    "nid": 1,
     "title": "python全栈",
     "w_category": 1,
     "pub_time": "2019-10-19",
@@ -63,15 +62,23 @@ book_obj = {
     "author_list": [1]
 }
 
-
+from SerDemo import models
 class BookSerializer(serializers.Serializer):
     nid = serializers.IntegerField(required=False)  # required=False 不校验nid
     title = serializers.CharField(max_length=32)
     CHOICES = ((1, "Python"), (2, "Go"), (3, "Linux"))
     category = serializers.ChoiceField(choices=CHOICES, source='get_category_display',read_only=True) #read_only=True  序列化展示用
-    w_category = serializers.ChoiceField(choices=CHOICES, read_only=True) #read_only=True  反序列化展示用  前端传入数据
+    w_category = serializers.ChoiceField(choices=CHOICES, write_only=True)
     pub_time = serializers.DateField()
     publish = PublisherSerializer(read_only=True)
     publish_nid = serializers.IntegerField(write_only=True) #
     author = AuthorSerializer(many=True,read_only=True)
     author_list = serializers.ListField(write_only=True) #
+
+
+
+    def create(self,validated_data):  # {'nid': 1, 'title': 'python全栈', 'w_category': 1, 'pub_time': '2019-10-19', 'publish_nid': 1, 'author_list': [1]}
+        book = models.Book.objects.create(title=validated_data['title'],category=validated_data['w_category'],pub_time=validated_data['pub_time'],
+                                   publish_id=validated_data['publish_nid'])
+        book.author.add(*validated_data['author_list'])
+        return book
