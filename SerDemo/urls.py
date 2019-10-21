a809d41761dcb7c19aae77f5cb4bc67ed6712978
kevinlandun@gmail.com <1qaz2wsx3edcQA1@>
2019-10-21 19:16:26 +0800

    DRF路由组件

--- SerDemo/urls.py
+++ SerDemo/urls.py
@@ -3,13 +3,17 @@
 
 from django.urls import path
 from SerDemo import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r"",views.BookModeViewSet)
 
 urlpatterns = [
-    # path('list/', views.BookView.as_view()),
-    # path('retrieve/<int:id>', views.BookEditView.as_view()),  # 2.0 以前版本写法   2.0 以后版本写法 <init:id>
+    # # # path('list/', views.BookView.as_view()),
+    # # # path('retrieve/<int:id>', views.BookEditView.as_view()),  # 2.0 以前版本写法   2.0 以后版本写法 <init:id>
+    # # path('list/', views.BookModeViewSet.as_view({"get": "list", "post": "create"})),
+    # # path('retrieve/<int:id>', views.BookModeViewSet.as_view({"get": "retrieve", "put": "update", "delete": "destroy"})),
     # path('list/', views.BookModeViewSet.as_view({"get": "list", "post": "create"})),
-    # path('retrieve/<int:id>', views.BookModeViewSet.as_view({"get": "retrieve", "put": "update", "delete": "destroy"})),
-    path('list/', views.BookModeViewSet.as_view({"get": "list", "post": "create"})),
-    path('retrieve/<int:pk>', views.BookModeViewSet.as_view({"get": "retrieve", "put": "update", "delete": "destroy"})),
+    # path('retrieve/<int:pk>', views.BookModeViewSet.as_view({"get": "retrieve", "put": "update", "delete": "destroy"})),
 ]
-
+urlpatterns += router.urls
