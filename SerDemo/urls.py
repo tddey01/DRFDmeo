8fe7bb16ebb5ce0542b7c131f360819e1a1ea0e5
kevinlandun@gmail.com <1qaz2wsx3edcQA1@>
2019-10-21 18:47:01 +0800

    视图函数组件总结

--- SerDemo/urls.py
+++ SerDemo/urls.py
@@ -7,7 +7,9 @@ from SerDemo import views
 urlpatterns = [
     # path('list/', views.BookView.as_view()),
     # path('retrieve/<int:id>', views.BookEditView.as_view()),  # 2.0 以前版本写法   2.0 以后版本写法 <init:id>
+    # path('list/', views.BookModeViewSet.as_view({"get": "list", "post": "create"})),
+    # path('retrieve/<int:id>', views.BookModeViewSet.as_view({"get": "retrieve", "put": "update", "delete": "destroy"})),
     path('list/', views.BookModeViewSet.as_view({"get": "list", "post": "create"})),
-    path('retrieve/<int:id>', views.BookModeViewSet.as_view({"get": "retrieve", "put": "update", "delete": "destroy"})),
+    path('retrieve/<int:pk>', views.BookModeViewSet.as_view({"get": "retrieve", "put": "update", "delete": "destroy"})),
 ]
 
--- SerDemo/views.py
+++ SerDemo/views.py
@@ -337,7 +337,7 @@ class BookEditView(RetrieveUpdateDestroyAPIView):
         return self.destroy(request,id)
 """
 
-# """
+"""
 # DRF 第六版 第三次次封装视图函数
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -485,17 +485,172 @@ from rest_framework.viewsets import ViewSetMixin # 框架自带
 class ModelViewSet(ViewSetMixin, GenericAPIView, ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
     pass
 
+
+# 案例
+# get-->self.list
 #
 # class BookModeViewSet(ViewSetMixin, GenericAPIView, ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
 #     query_set = models.Book.objects.all()
 #     serializer_class = serializers.BookSerializer
-# get-->self.list
+#
 
-# from rest_framework.viewsets import ModelViewSet
+
 
 class BookModeViewSet(ModelViewSet):
     query_set = models.Book.objects.all()
     serializer_class = serializers.BookSerializer
 
 
+"""
+
+# """
+# DRF 第七版 视图组件总结 优化
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from SerDemo import serializers
+from SerDemo import models
+
+
+class GenericAPIView(APIView):
+    query_set = None
+    serializer_class = None
+
+    def get_queryset(self):
+        '''
+
+        :return:
+        '''
+        return self.query_set
+
+    def get_serializer(self, *args, **kwargs):
+        '''
+
+        :param args:
+        :param kwargs:
+        :return:
+        '''
+        return self.serializer_class(*args, **kwargs)
+
+
+class ListModelMixin(object):
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        ret = self.get_serializer(queryset, many=True)
+        return Response(ret.data)
+
+
+class CreateModelMixin(object):
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class ListCreateModelMixiin(GenericAPIView, ListModelMixin, CreateModelMixin):
+    pass
+
+
+class BookView(ListCreateModelMixiin):
+    query_set = models.Book.objects.all()
+    serializer_class = serializers.BookSerializer
+
+    def get(self, request):  # 查看数据
+        '''
+
+        :param request:
+        :return:
+        '''
+        return self.list(request)
+
+    def post(self, request):  # 添加数据
+        '''
+
+        :param request:
+        :return:
+        '''
+        return self.create(request)
+
+
+class RetrieveModelMixin(object):
+    def retrieve(self, request, id):
+        book_obj = self.get_queryset().filter(nid=id).first()
+        ret = self.get_serializer(book_obj)
+        return Response(ret.data)
+
+
+class UpdateModelMixin(object):
+    def update(self, request, id):
+        book_obj = self.get_queryset().filter(nid=id).first()
+        serializer = self.get_serializer(book_obj, data=request.data, partial=True)  # partial=True  允许部分更新
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class DestroyModelMixin(object):
+
+    def destroy(self, request, id):
+        book_obj = self.get_queryset().filter(nid=id).first()
+        book_obj.delete()
+        return Response('delete')
+
+
+class RetrieveUpdateDestroyAPIView(GenericAPIView, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
+    pass
+
+
+class BookEditView(RetrieveUpdateDestroyAPIView):
+    query_set = models.Book.objects.all()
+    serializer_class = serializers.BookSerializer
+
+    def get(self, request, id):  # 查看单条数据
+        '''
+
+        :param request:
+        :param id:
+        :return:
+        '''
+        return self.retrieve(request, id)
+
+    def put(self, request, id):  # 修改数据
+        '''
+
+        :param request:
+        :param id:
+        :return:
+        '''
+        return self.update(request, id)
+
+    def delete(self, request, id):  # 删除数据
+        '''
+
+        :param request:
+        :param id:
+        :return:
+        '''
+        return self.destroy(request, id)
+
+
+from rest_framework.viewsets import ViewSetMixin # 框架自带
+
+
+from rest_framework.viewsets import  ModelViewSet
+
+
+
+class BookModeViewSet(ModelViewSet):
+    queryset = models.Book.objects.all()  #query_set 使用框架自带的 queryset
+    serializer_class = serializers.BookSerializer
+
+
+from rest_framework import views  # 框架提供  --->  APIView(View):
+from rest_framework import generics    #  ---> GenericAPIView(views.APIView):
+from rest_framework import mixins      # --->
+from rest_framework import viewsets
 # """
