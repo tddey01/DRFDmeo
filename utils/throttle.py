e696c724c65041e21f14c0d0c919a74eb22eabe2
kevinlandun@gmail.com <1qaz2wsx3edcQA1@>
2019-10-22 15:22:24 +0800

    频率控制逻辑

--- utils/throttle.py
+++ utils/throttle.py
@@ -5,6 +5,16 @@ from  rest_framework.throttling import  BaseThrottle
 class MyTrottle(BaseThrottle):
     def allow_request(self, request, view):
         # 实现限流的逻辑
+        # 以IP访问
+        # 访问列表 {IP:[tiem1,time2,time3]}
+        # 获取请求的IP地址
+        # 判断IP地址是否在访问列表
+            #  不在 需要给访问列表添加key，value
+            #  在 需要把这个IP的访问记录 把当前时间加入到列表
+        # 确保列表里最新访问时间以及最老的访问时间差 是1分钟
+        # 得到列表长度，判断是否是允许的次数
+        # 返回需要再等多久才能访问
+
         pass
 
     def wait(self):
